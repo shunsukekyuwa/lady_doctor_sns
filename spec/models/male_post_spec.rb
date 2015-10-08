b3805322c370523930d37c893f360c0099bb7429
Shunsuke Kyuwa <s.kyuwa9@gmail.com>
2015-10-08 23:14:58 +0900

    male_post model test fixed

--- spec/models/male_post_spec.rb
+++ spec/models/male_post_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 describe MalePost do
   #male_idをもつmaleが存在し、かつ、contentが存在すると有効な状態であること
-  it "is valid with the male with a certain male_id and with a content"
+  it "is valid with the male with a certain male_id and with a content" do
+    malepost = MalePost.new(
+      male_id: 6,
+      content: 'hoge')
+    expect(malepost).to be_valid
+  end
   #male_idをもつmaleが存在しなければ無効な状態であること
-  it "is invalid without the male with a certain male_id"
+  it "is invalid without the male with a certain male_id" do
+    male_post = MalePost.new(male_id: nil)
+    male_post.valid?
+    expect(male_post.errors[:male_id]).to include("can't be blank")
+  end
   #contentがなければ無効な状態であること
-  it "is invalid without a content"
+  it "is invalid without a content" do
+    male_post = MalePost.new(content: nil)
+    male_post.valid?
+    expect(male_post.errors[:content]).to include("can't be blank")
+  end
 end
